82f4546079fafaad24efe6915e36b8d1fc9dd51b
Aaron Austin <aaron@aaronaustin.com>
2018-04-22 22:08:22 -0400

    sharing popup

--- src/components/sharing/sharing.js
+++ src/components/sharing/sharing.js
@@ -6,12 +6,8 @@ import IconsBrand from '../icons/iconsBrand';
 const Sharing = (props) => {
     const baseUrl = "http://www.aaronaustin.com";
     const fbAppId = "1773314012731497";
-    // const fbBase = "https://www.facebook.com/sharer.php?u=";
     const twBase = "https://twitter.com/intent/tweet?url=";
     const fbBase = "https://www.facebook.com/dialog/share?app_id=" + fbAppId + "&display=popup&href="
-
-  // https://www.facebook.com/dialog/share?app_id={app_id}&display=popup&href={ url }& redirect_uri={ redirect_url }
-
     
     const twText = "&text=" + encodeURIComponent(props.title);
     const twHash = "&hashtags=music,folk";
@@ -19,13 +15,17 @@ const Sharing = (props) => {
     const fbShareUrl = fbBase + encodeURIComponent(baseUrl + props.shareUrl);
     const twShareUrl = twBase + encodeURIComponent(baseUrl + props.shareUrl) + twText + twHash;
 
+    const popUp = (shareUrl) => {
+      window.open(shareUrl,'test','width=550,height=450,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
+    };
+
     console.log(props);
     return (
       <span className={styles.container}>
-        <a href={fbShareUrl} target="_blank">
+        <a onClick={() => {popUp(fbShareUrl)}}>
           <IconsBrand name={'facebook'}/>
         </a>
-        <a href={twShareUrl} target="_blank">
+        <a onClick={() => { popUp(twShareUrl) }}>
           <IconsBrand name={'twitter'}/>
         </a>
       </span>
